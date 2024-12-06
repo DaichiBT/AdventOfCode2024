6de408def9f946cfaa9450cee0c423fcebf89675
DaichiBT <144473574+DaichiBT@users.noreply.github.com>
2024-12-06 13:59:53 -0500

    Add files via upload

---
+++ Day04.py
@@ -0,0 +1,31 @@
+import re
+
+
+def get_file_data(file_name):
+    f = open(file_name)
+    data = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+
+
+file_data = get_file_data("Day04Input")
+
+grid = []
+for line in file_data:
+    row = []
+    for letter in line:
+        row.append(letter)
+    grid.append(row)
+
+count = 0
+x_count = 0
+
+for r in range(len(grid)):
+    for c in range(len(grid[r])):
+        if grid[r][c] == "X":
+            x_count += 1
+            if grid[r][c + 1] == "M" or grid[r][c - 1] or grid[r + 1][c] or grid[r - 1][c] or grid[r + 1][c + 1] == "M" or grid[r + 1][c - 1] == "M" or grid[r - 1][c + 1] == "M" or grid[r - 1][c - 1] == "M":
+                
+
+print(x_count)
