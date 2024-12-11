583a4f2aff1d3f02c56b7e5595b6eae40650d1f6
DaichiBT <144473574+DaichiBT@users.noreply.github.com>
2024-12-11 14:31:37 -0500

    Add files via upload

--- Day01.py
+++ Day01.py
@@ -6,20 +6,20 @@ def get_file_data(file_name):
     return data
 
 def get_total(file_data):
-    left_list = []
-    right_list = []
+    llist = []
+    rlist = []
 
     for line in file_data:
         numbers = line.split()
-        left_list.append(int(numbers[0]))
-        right_list.append(int(numbers[1]))
+        llist.append(int(numbers[0]))
+        rlist.append(int(numbers[1]))
 
-    left_list.sort()
-    right_list.sort()
+    llist.sort()
+    rlist.sort()
 
     total = 0
-    for i in range(len(left_list)):
-        total += abs(left_list[i] - right_list[i])
+    for i in range(len(llist)):
+        total += abs(llist[i] - rlist[i])
 
     return total
 
