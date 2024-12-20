f8adee679a93c36279455d4c52b39013a718b3a6
DaichiBT <144473574+DaichiBT@users.noreply.github.com>
2024-12-19 22:29:08 -0500

    Add files via upload

--- Day02.py
+++ Day02.py
@@ -6,39 +6,34 @@ def get_file_data(file_name):
     return data
 
 
+def check(num):
+    levels = list(map(int, num.split()))
+
+    for i in range(len(levels) - 1):
+        if abs(levels[i] - levels[i + 1]) < 1 or abs(levels[i] - levels[i + 1]) > 3:
+            return False
+
+    increasing = True
+    decreasing = True
+    for i in range(len(levels) - 1):
+        if levels[i] > levels[i + 1]:
+            increasing = False
+        if levels[i] < levels[i + 1]:
+            decreasing = False
+    return increasing or decreasing
+
+
+def count_safe_reports(file_data):
+    safe_count = 0
+
+    for report in file_data:
+        if check(report):
+            safe_count += 1
+
+    return safe_count
+
+
 file_data = get_file_data("Day02Input")
 
-
-# Safe if -
-# The levels are either all increasing or all decreasing.
-# Any two adjacent levels differ by at least one and at most three.
-
-def is_decreasing(nums):
-    return nums == sorted(nums, reverse=True)
-
-
-def is_increasing(nums):
-    return nums == sorted(nums, reverse=False)
-
-
-def in_range(lower, upper, nums):
-    inRange = True
-    for elem in nums:
-        if int(elem) <= lower or int(elem) >= upper:
-            return False
-    return inRange
-
-
-count = 0
-
-for element in file_data:
-    iElement = element.split(" ")
-    diff_list = []
-    for x, y in zip(iElement[0::], iElement[1::]):
-        diff_list.append(abs(int(y) - int(x)))
-
-    if is_increasing(iElement) or is_decreasing(iElement):
-        if in_range(1, 3, diff_list):
-            count += 1
-
-print(count)
+safe_reports = count_safe_reports(file_data)
+print(safe_reports)
